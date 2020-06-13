f617ea21f652b8d474d96ee4dc9f8bbda116572e
Luqman Shofuleji <luqmanshof@gmail.com>
2020-06-13 08:35:45 +0100

    Added gitignore, procfile, node engine version and update ports

--- app.js
+++ app.js
@@ -142,6 +142,13 @@ app.post("/delete", function (req, res) {
 
 });
 
-app.listen(3000, function () {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+// app.listen(port);
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
